784d174f824d1b12e64d49042d114694c8c9a195
qwp0905 <qwp0905@gmail.com>
2025-02-06 21:11:17 +0900

    initial commit

---
+++ src/heap.ts
@@ -0,0 +1,57 @@
+export class MaxHeap<T> {
+  private readonly heap: T[] = []
+
+  get length(): number {
+    return this.heap.length
+  }
+
+  push(value: T): void {
+    this.heap.push(value)
+    let i = this.heap.length - 1
+    let p: number
+    while (i > 0 && this.heap[(p = Math.floor((i - 1) / 2))] < value) {
+      this.swap(i, p)
+      i = p
+    }
+  }
+
+  pop(): T | undefined {
+    if (this.heap.length === 0) {
+      return undefined
+    }
+
+    const last = this.heap.pop()!
+    if (this.heap.length === 0) {
+      return last
+    }
+
+    const root = this.heap[0]
+    this.heap[0] = last
+    let i = 0
+    let left
+    while ((left = i * 2 + 1) < this.heap.length) {
+      const right = left + 1
+      let max = left
+      if (right < this.heap.length && this.heap[right] > this.heap[left]) {
+        max = right
+      }
+      if (this.heap[i] >= this.heap[max]) {
+        break
+      }
+      this.swap(i, max)
+      i = max
+    }
+
+    return root
+  }
+
+  peek(): T | undefined {
+    return this.heap[0]
+  }
+
+  private swap(index1: number, index2: number): void {
+    const temp = this.heap[index1]
+    this.heap[index1] = this.heap[index2]
+    this.heap[index2] = temp
+  }
+}
