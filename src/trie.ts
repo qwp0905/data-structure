cfdbd917f4f09ced225af464e7101632bf656f85
qwp0905 <qwp0905@gmail.com>
2025-02-07 21:27:19 +0900

    add trie

---
+++ src/trie.ts
@@ -0,0 +1,63 @@
+export class Trie {
+  private readonly children: Trie[] = []
+  constructor(
+    private word: string = "",
+    private isWord = false,
+    ...children: Trie[]
+  ) {
+    this.children = children
+  }
+
+  add(word: string) {
+    if (word === this.word) {
+      this.isWord = true
+      return
+    }
+
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i]
+      let substr = ""
+      const minLen = Math.min(word.length, child.word.length)
+      for (let j = 0; j < minLen; j++) {
+        if (word[j] !== child.word[j]) {
+          break
+        }
+
+        substr += word[j]
+      }
+
+      if (substr.length <= this.word.length) {
+        continue
+      }
+      if (substr.length === word.length) {
+        this.children[i] = new Trie(substr, true, child)
+        return
+      }
+      if (substr.length === child.word.length) {
+        child.add(word)
+        return
+      }
+
+      const node = new Trie(substr, false, child)
+      this.children[i] = node
+      node.add(word)
+      return
+    }
+
+    this.children.push(new Trie(word, true))
+  }
+
+  has(word: string): boolean {
+    if (this.isWord) {
+      return true
+    }
+
+    for (const child of this.children) {
+      if (word.startsWith(child.word)) {
+        return child.has(word)
+      }
+    }
+
+    return false
+  }
+}
