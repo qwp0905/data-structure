195c3ba2e556b2eabb63108e3835470a56daeb5c
qwp0905 <qwp0905@gmail.com>
2025-03-03 19:35:57 +0900

    add more test

--- test/list/skip.spec.ts
+++ test/list/skip.spec.ts
@@ -138,6 +138,30 @@ describe("SkipList", () => {
     }
   })
 
+  it("should insert and delete at many key 2", () => {
+    for (let i = 1; i <= 100; i++) {
+      expect(list.insert({ key: i })).toBeUndefined()
+    }
+
+    for (let i = 1; i <= 100; i++) {
+      expect(list.get(i)).toEqual({ key: i })
+    }
+    expect(list.length).toBe(100)
+
+    const delete_list = [9, 2, 1, 5, 89, 23]
+    for (const key of delete_list) {
+      expect(list.delete(key)).toEqual({ key })
+    }
+
+    for (let i = 1; i <= 100; i++) {
+      if (delete_list.includes(i)) {
+        expect(list.get(i)).toBeUndefined()
+      } else {
+        expect(list.get(i)).toEqual({ key: i })
+      }
+    }
+  })
+
   it("should iterate", () => {
     for (let i = 100; i >= 1; i--) {
       expect(list.insert({ key: i })).toBeUndefined()
