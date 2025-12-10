2fd88f2cbda4f8c0c672e118b1176d9fe01f9322
qwp0905 <qwp0905@gmail.com>
2025-12-10 20:31:33 +0900

    fix typo

--- src/sort/merge.ts
+++ src/sort/merge.ts
@@ -1,32 +1,34 @@
-export function mergeSort<T>(arr: T[], compare: (a: T, b: T) => number): T[] {
-  const temp: T[] = []
-  for (let i = 1; i < arr.length; i <<= 1) {
-    for (let left = 0; left + i < arr.length; left += i << 1) {
-      const right = left + i
-      const end = Math.min(right + i, arr.length)
-      let begin = left
-      let count = left
-      let center = right
+export class MergeSortArray<T> extends Array<T> {
+  sort(compareFn: (a: T, b: T) => number = (a, b) => (a > b ? 1 : a < b ? -1 : 0)): this {
+    const temp: T[] = []
+    for (let i = 1; i < this.length; i <<= 1) {
+      for (let left = 0; left + i < this.length; left += i << 1) {
+        const right = left + i
+        const end = Math.min(right + i, this.length)
+        let begin = left
+        let count = left
+        let center = right
 
-      while (begin < right && center < end) {
-        if (compare(arr[begin], arr[center]) < 0) {
-          temp[count++] = arr[begin++]
-        } else {
-          temp[count++] = arr[center++]
+        while (begin < right && center < end) {
+          if (compareFn(this[begin], this[center]) < 0) {
+            temp[count++] = this[begin++]
+          } else {
+            temp[count++] = this[center++]
+          }
+        }
+
+        while (begin < right) {
+          temp[count++] = this[begin++]
+        }
+        while (center < end) {
+          temp[count++] = this[center++]
+        }
+
+        for (let k = left; k < end; k += 1) {
+          this[k] = temp[k]
         }
       }
-
-      while (begin < right) {
-        temp[count++] = arr[begin++]
-      }
-      while (center < end) {
-        temp[count++] = arr[center++]
-      }
-
-      for (let k = left; k < end; k += 1) {
-        arr[k] = temp[k]
-      }
     }
+    return this
   }
-  return arr
 }
