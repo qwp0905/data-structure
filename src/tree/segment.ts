dbb411cfc87de34f256c075f2a5f4af5824a1e58
qwp0905 <qwp0905@gmail.com>
2026-02-01 11:27:21 +0900

    add lazy segment tree

--- src/tree/segment.ts
+++ src/tree/segment.ts
@@ -20,7 +20,7 @@ export class SegmentTree {
       if (l & 1) {
         s += this.table[l++]
       }
-      if (!(r & 1)) {
+      if (~r & 1) {
         s += this.table[r--]
       }
     }
@@ -35,5 +35,93 @@ export class SegmentTree {
       this.table[p] = this.table[c] + this.table[c ^ 1]
       c = p
     }
+  }
+}
+
+export class LazySegmentTree {
+  private readonly size: number
+  private readonly height: number
+  private readonly table: number[]
+  private readonly lazy: number[]
+
+  constructor(data: number[]) {
+    this.height = Math.ceil(Math.log2(data.length))
+    this.size = 1 << this.height
+    this.table = Array(this.size << 1).fill(0)
+    this.lazy = Array(this.size).fill(0)
+
+    for (let i = 0; i < this.size; i += 1) {
+      this.table[this.size + i] = data[i]
+    }
+    for (let i = this.size - 1; i > 0; i -= 1) {
+      this.pull(i)
+    }
+  }
+  update(a: number, b: number, v: number) {
+    const left = a + this.size
+    const right = b + this.size
+    for (let i = this.height; i > 0; i -= 1) {
+      if ((left >>> i) << i !== left) {
+        this.push(left >>> i, 1 << (i - 1))
+      }
+      if (((right + 1) >>> i) << i !== right + 1) {
+        this.push(right >>> i, 1 << (i - 1))
+      }
+    }
+
+    for (let l = left, r = right, k = 1; l <= r; l >>>= 1, r >>>= 1, k <<= 1) {
+      if (l & 1) {
+        this.apply(l++, v, k)
+      }
+      if (~r & 1) {
+        this.apply(r--, v, k)
+      }
+    }
+
+    for (let i = 1; i <= this.height; i += 1) {
+      if ((left >>> i) << i !== left) {
+        this.pull(left >>> i)
+      }
+      if (((right + 1) >>> i) << i !== right + 1) {
+        this.pull(right >>> i)
+      }
+    }
+  }
+  query(a: number, b: number) {
+    const left = a + this.size
+    const right = b + this.size
+    for (let i = this.height; i > 0; i -= 1) {
+      if ((left >>> i) << i !== left) {
+        this.push(left >>> i, 1 << (i - 1))
+      }
+      if (((right + 1) >>> i) << i !== right + 1) {
+        this.push(right >>> i, 1 << (i - 1))
+      }
+    }
+
+    let s = 0
+    for (let l = left, r = right; l <= r; l >>>= 1, r >>>= 1) {
+      if (l & 1) {
+        s += this.table[l++]
+      }
+      if (~r & 1) {
+        s += this.table[r--]
+      }
+    }
+    return s
+  }
+  private apply(i: number, v: number, k: number) {
+    this.table[i] += v * k
+    if (i < this.size) {
+      this.lazy[i] += v
+    }
+  }
+  private push(i: number, k: number) {
+    this.apply(i << 1, this.lazy[i], k)
+    this.apply((i << 1) | 1, this.lazy[i], k)
+    this.lazy[i] = 0
+  }
+  private pull(i: number) {
+    this.table[i] = this.table[i << 1] + this.table[(i << 1) | 1]
   }
 }
--- test/tree/segment.spec.ts
+++ test/tree/segment.spec.ts
@@ -1,4 +1,4 @@
-import { SegmentTree } from "../../src/tree/segment"
+import { LazySegmentTree, SegmentTree } from "../../src/tree/segment"
 
 describe("SegmentTree", () => {
   let tree: SegmentTree
@@ -17,5 +17,26 @@ describe("SegmentTree", () => {
     expect(tree.query(0, 5)).toBe(43)
     tree.update(4, 0)
     expect(tree.query(0, 5)).toBe(34)
+  })
+})
+
+describe("LazySegmentTree", () => {
+  let tree: LazySegmentTree
+
+  it("should query", () => {
+    tree = new LazySegmentTree([1, 3, 5, 7, 9, 11])
+    expect(tree.query(1, 3)).toBe(15)
+    expect(tree.query(0, 5)).toBe(36)
+    expect(tree.query(2, 4)).toBe(21)
+  })
+
+  it("should update range", () => {
+    tree = new LazySegmentTree([1, 3, 5, 7, 9, 11])
+    expect(tree.query(1, 3)).toBe(15)
+    tree.update(1, 3, 10)
+    expect(tree.query(1, 3)).toBe(45)
+    expect(tree.query(0, 5)).toBe(66)
+    tree.update(0, 5, 5)
+    expect(tree.query(0, 5)).toBe(96)
   })
 })
